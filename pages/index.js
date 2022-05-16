6a24325f691c4daabd0c6ddea62ab28e24b2c318
GraniteMask <ratnadeepastronomy@gmail.com>
2022-05-16 21:20:24 +0530

    name and liked

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { IconButton } from '@mui/material';
+import { Avatar, IconButton } from '@mui/material';
 import { useEffect, useState } from 'react';
 import Layout from '../components/Layout'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
@@ -13,49 +13,13 @@ export default function Home({imageInfo}) {
   useEffect(()=>{
     setItemList(imageInfo)
   },[])
-  
+
   console.log(itemList)
-  const _items = [
-      {
-          player: {
-              title: 'Efren Reyes',
-              desc: 'Known as "The Magician", Efren Reyes is well regarded by many professionals as the greatest all around player of all time.',
-              image: 'https://i.postimg.cc/RhYnBf5m/er-slider.jpg',
-          },
-      },
-      {
-          player: {
-              title: "Ronnie O'Sullivan",
-              desc: "Ronald Antonio O'Sullivan is a six-time world champion and is the most successful player in the history of snooker.",
-              image: 'https://i.postimg.cc/qBGQNc37/ro-slider.jpg',
-          },
-      },
-      {
-          player: {
-              title: 'Shane Van Boening',
-              desc: 'The "South Dakota Kid" is hearing-impaired and uses a hearing aid, but it has not limited his ability.',
-              image: 'https://i.postimg.cc/cHdMJQKG/svb-slider.jpg',
-          },
-      },
-      {
-          player: {
-              title: 'Mike Sigel',
-              desc: 'Mike Sigel or "Captain Hook" as many like to call him is an American professional pool player with over 108 tournament wins.',
-              image: 'https://i.postimg.cc/C12h7nZn/ms-1.jpg',
-          },
-      },
-      {
-          player: {
-              title: 'Willie Mosconi',
-              desc: 'Nicknamed "Mr. Pocket Billiards," Willie Mosconi was among the first Billiard Congress of America Hall of Fame inductees.',
-              image: 'https://i.postimg.cc/NfzMDVHP/willie-mosconi-slider.jpg',
-          },
-      },
-  ];
+  
 
 //   console.log(_items)
-  const length = _items.length;
-  _items.push(..._items);
+  const length = 5;
+//   _items.push(..._items);
 
   const sleep = (ms = 0) => {
       return new Promise((resolve) => setTimeout(resolve, ms));
@@ -95,6 +59,8 @@ export default function Home({imageInfo}) {
               <div className="carousel-slide-item__body">
                   {/* <h4>{item.player.title}</h4>
                   <p>{item.player.desc}</p> */}
+                  <Avatar alt={item.player.name} src={item.player.profile_image} />
+                  <h4>{item.player.name}</h4>
               </div>
           </li>
       );
@@ -173,6 +139,9 @@ export async function getServerSideProps({params}){
         for(var i=0; i<data.length; i++){
             var player = {}
             player.image = data[i].urls.raw
+            player.like = data[i].likes
+            player.profile_image = data[i].user.profile_image.medium
+            player.name = data[i].user.name
             console.log(player)
             var playerLarge = {}
             playerLarge = {player: player}
